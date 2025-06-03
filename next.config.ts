70ebde1a9ff70805a194545fef60af90e1aed374
Akash <ashish71570@gmail.com>
2025-06-03 10:29:47 +0530

    eslint Disabled

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["cdn-icons-png.flaticon.com"],
-  }
+  },
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
